af26c1456c0cd97e898fb353c37b56c1a47615ea
Miralhaz <lucas.silva051@sptech.school>
2025-10-22 18:38:53 -0300

    Mundaça Python: Deixar tudo em um arquivo

--- Captura_Servidor.py
+++ Captura_Servidor.py
@@ -4,6 +4,9 @@ import pandas as pd
 import mysql.connector as mysql
 from datetime import datetime
 import time
+
+
+
 #Esse script pega o nome do servidor e joga para o bancoalun
 print("Credenciais do banco de dados MySQL")
 opcaouser = input("Digite seu usuario:")
@@ -21,8 +24,7 @@ try:
     cur = conexao.cursor()
 
 
-
-except mysql.connector.Error as err:
+except mysql.Error as err:
     print(f"Erro ao conectar ao MySQL: {err}")
     exit()
 
@@ -35,7 +37,11 @@ conexao.commit()
 print("\n=== Servidor Capturado ===")
 #fim do script que captura e joga o nome do servidor para o banco
 
+
+
 time.sleep(1)
+
+
 
 #Esse script pega todos os componentes que estão em atividade e joga para o banco
 # Informações do SO
@@ -53,7 +59,6 @@ print(processador)
 print("\n")
 print("\n=== Iniciando Captura de componentes ===")
 
-
 uso = psutil.cpu_percent(interval=1) 
 ramTotal = round(psutil.virtual_memory().total / (1024**3),2)
 ramUsada = psutil.virtual_memory().percent
@@ -61,9 +66,6 @@ discoTotal = round(psutil.disk_usage("/").total / (1024**3),2)
 discoUsado = psutil.disk_usage("/").percent
 
 print(f"Nome da Máquina: {nomeMaquina} | CPU: {uso}% | Ram total: {ramTotal}GB | Ram em Uso: {ramUsada}% | Disco total: {discoTotal}GB | Disco em uso: {discoUsado}%")
-
-
-
 
 cur.execute(f"insert into componente (tipo, fk_servidor) select 'CPU', id from servidor where nome_maquina = '{nomeMaquina}';")
 conexao.commit()
@@ -75,7 +77,11 @@ conexao.commit()
 print("\n=== Componentes capturados ===")
 #fim do script que captura informações do componente
 
+
+
 time.sleep(1)
+
+
 
 #Esse script pega as especificações dos componentes, como quantidade de total de ram
 contador = 1
@@ -146,17 +152,113 @@ dados = {
     "Data e hora da captura": timestamp
 }
 
-
-
-
 data.append(dados)
-
-
 
 df1 = pd.DataFrame(data = data)
 
 df1.to_csv('EspecificacoesHardware.csv',sep=';')
 
 print("------- Especificações capturadas -------")
-
 # Fim do script de captura de especificações
+
+
+
+time.sleep(1)
+
+
+
+#Inicio do script de metricas para o banco de dados
+duracao = int(input("Digite a duração da captura: "))
+contador = 0
+
+arquivo_csv = "dados.csv"
+processos = "processos.csv"
+
+processos = []
+data = []
+
+print("\nIniciando monitoramento...")
+print("\n------- CAPTURA DE CPU, RAM E DISCO -------")
+
+while contador < duracao:
+    user = psutil.users()[0].name
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    cpu = psutil.cpu_percent()  
+    ram = psutil.virtual_memory().percent  
+    disco = psutil.disk_usage("/").percent  
+    temperatura_cpu = psutil.sensors_temperatures(fahrenheit = False)
+    temperatura_disco = psutil.sensors_temperatures(fahrenheit = False)
+    memoria_swap = round(psutil.swap_memory().used / (1024 * 1024), 2)
+    processos_maquina = psutil.process_iter()
+
+    processos_list = list(processos_maquina)
+    quantidade_processos = len(processos_list)
+
+    local_cpu = temperatura_cpu['coretemp']
+    cpu_sensor = local_cpu[0]
+    temperatura_cpu_atual = cpu_sensor.current
+
+    local_disco = temperatura_disco['nvme']
+    disco_sensor = local_disco[0]
+    temperatura_disco_atual = disco_sensor.current
+
+    dado = {
+        'user': user
+        ,'timestamp':timestamp
+        ,'cpu': cpu
+        ,'ram': ram
+        ,'disco': disco
+        ,'temperatura_cpu': temperatura_cpu_atual
+        ,'temperatura_disco': temperatura_disco_atual
+        ,'memoria_swap': memoria_swap
+        ,'quantidade_processos': quantidade_processos
+    }
+ 
+    # Salva no CSV
+    data.append(dado)
+
+    print(f"\n Usuário: {user} | {timestamp} | CPU: {cpu}% | RAM: {ram}% | Disco: {disco}% | Temperatura CPU: {temperatura_cpu_atual}ºC | Temperatura Disco: {temperatura_disco_atual}ºC | Memória Swap: {memoria_swap}% | Quantidade de processos: {quantidade_processos}")
+  
+    for proc in psutil.process_iter():
+        dado = {
+        'timestamp':timestamp
+        ,'processo': proc.name()
+        ,'pid': proc.pid
+        ,'cpu':proc.cpu_percent()
+        ,'ram': round(proc.memory_percent(),4)
+    }
+        processos.append(dado)
+
+    # DADOS CPU
+    cur.execute(f"insert into medicao (nome_medicao, medicao, unidade_medida, fk_componente) select 'Temperatura', '{temperatura_cpu_atual}', '°C', id from componente where tipo = 'CPU'")
+    conexao.commit()
+    cur.execute(f"insert into medicao (nome_medicao, medicao, unidade_medida, fk_componente) select 'Uso', '{cpu}', '%', id from componente where tipo = 'CPU'")
+    conexao.commit()
+
+    # DADOS RAM
+    cur.execute(f"insert into medicao (nome_medicao, medicao, unidade_medida, fk_componente) select 'RAM', '{ram}','%', id from componente where tipo = 'CPU'")
+    conexao.commit()
+
+    # DADOS DISCO
+    cur.execute(f"insert into medicao (nome_medicao, medicao, unidade_medida, fk_componente) select 'Temperatura', '{temperatura_disco_atual}', '°C', id from componente where tipo = 'DISCO'")
+    conexao.commit()
+    cur.execute(f"insert into medicao (nome_medicao, medicao, unidade_medida, fk_componente) select 'Uso', '{disco}', '%', id from componente where tipo = 'DISCO'")
+    conexao.commit()
+
+    contador+=1
+    time.sleep(1)
+
+df1 = pd.DataFrame(data = data)
+
+df1.to_csv('data.csv',sep=';')
+
+df = pd.DataFrame(data = processos)
+
+df.to_csv('processos.csv',sep=';')
+
+print("\n------- CAPTURA DE PROCESSOS -------\n")
+print(df) 
+
+
+print("Finalizando monitoramento...")
+# Fim do script de capturar metricas
